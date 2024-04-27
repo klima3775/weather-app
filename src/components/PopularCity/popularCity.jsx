bf6a72f515290be7b938ef7d445350b8284e59d8
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-04-28 02:26:10 +0300

    style search

---
+++ src/components/PopularCity/popularCity.jsx
@@ -0,0 +1,13 @@
+const PopularCity = () => {
+  return (
+    <div>
+      <h2>Popular Cities</h2>
+      <ul>
+        <li>Paris</li>
+        <li>London</li>
+        <li>New York</li>
+      </ul>
+    </div>
+  );
+};
+export default PopularCity;
