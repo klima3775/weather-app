196d4a9cefd586965c90fcaa5437631b3562fc9f
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-05-01 03:31:59 +0300

    deleted unnecessary folders and files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import PopularCity from "./components/PopularCity/popularCity";
-import RenderWeather from "./components/RenderWeather/RenderWeather";
+// import RenderWeather from "./components/RenderWeather/RenderWeather";
+import { useState } from "react";
+import SearchForm from "./components/SearchForm/SearchForm";
+import WeatherItem from "./components/WeatherItem/WeatherItem";
+import { fetchWeatherData } from "./utils/response";
 import "./App.css";
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleSearch = async (city) => {
+    try {
+      const data = await fetchWeatherData(city);
+      setWeatherData(data);
+    } catch (error) {
+      console.error(error);
+      setWeatherData(null);
+    }
+  };
   return (
     <div className="App">
       <PopularCity />
-      <RenderWeather />
+      <SearchForm onSearch={handleSearch} />
+      <WeatherItem weatherData={weatherData} />
     </div>
   );
 }
