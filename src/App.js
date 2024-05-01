e33dc7234eed003e4ca2b94bd36fb9e0e98c466a
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-05-01 15:57:14 +0300

    Add modal window

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
+import React, { useState } from "react";
 import PopularCity from "./components/PopularCity/popularCity";
-// import RenderWeather from "./components/RenderWeather/RenderWeather";
-import { useState } from "react";
 import SearchForm from "./components/SearchForm/SearchForm";
 import WeatherItem from "./components/WeatherItem/WeatherItem";
 import { fetchWeatherData } from "./utils/response";
@@ -8,16 +7,40 @@ import "./App.css";
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleCitySelect = async (city) => {
-    const data = await fetchWeatherData(city);
-    setWeatherData(data);
+    try {
+      const data = await fetchWeatherData(city);
+      setWeatherData(data);
+      setError(null);
+      setIsModalOpen(false);
+    } catch (error) {
+      setError(error.message);
+      setIsModalOpen(true);
+    }
   };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="App">
       <PopularCity onCityClick={handleCitySelect} />
       <SearchForm onSearch={handleCitySelect} />
       <WeatherItem weatherData={weatherData} />
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <p>{error}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
