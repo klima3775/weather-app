9975a13428230e170570c630be55aed278942d21
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-05-16 02:33:14 +0300

    prepared variables for the modal window

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,13 +3,19 @@ import "./Modal.scss";
 
 function Modal({ closeModal, error }) {
   return (
+    //     <div className="modal">
+    //       <div className="modal-content">
+    //         <span className="close" onClick={closeModal}>
+    //           &times;
+    //         </span>
+    //         <p>{error}</p>
+    //       </div>
+    //     </div>
+    //   );
     <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={closeModal}>
-          &times;
-        </span>
-        <p>{error}</p>
-      </div>
+      <button className="close" onClick={closeModal}>
+        close
+      </button>
     </div>
   );
 }
