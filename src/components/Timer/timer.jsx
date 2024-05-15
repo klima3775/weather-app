fc3030f8cbc6cebc375151c396cdb742bdb352a4
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-05-15 03:09:37 +0300

    Add animation and stile for Timer

--- src/components/Timer/timer.jsx
+++ src/components/Timer/timer.jsx
@@ -4,10 +4,23 @@ import "./timer.scss";
 
 const Timer = () => {
   const [time, setTime] = useState(new Date());
+  const [prevMinute, setPrevMinute] = useState(time.getMinutes());
+  const [prevHour, setPrevHour] = useState(time.getHours());
+  const [animate, setAnimate] = useState(false);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setTime(new Date());
+      const currentTime = new Date();
+      const currentMinute = currentTime.getMinutes();
+
+      if (currentMinute !== prevMinute) {
+        setAnimate(true);
+        setPrevMinute(currentMinute);
+      } else {
+        setAnimate(false);
+      }
+
+      setTime(currentTime);
     }, 1000);
 
     return () => {
@@ -17,8 +30,14 @@ const Timer = () => {
 
   return (
     <div className="timer">
-      <Moment format="HH:mm:ss">{time}</Moment>
+      <div className={`time-card ${animate ? "flip" : ""}`}>
+        <Moment format="HH">{time}</Moment>
+      </div>
+      <div className={`time-card ${animate ? "flip" : ""}`}>
+        <Moment format="mm">{time}</Moment>
+      </div>
     </div>
   );
 };
+
 export default Timer;
